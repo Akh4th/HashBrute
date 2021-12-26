ee44cbf3d836f2e63380b672531f611d18c80566
Akh4th <68875577+Akh4th@users.noreply.github.com>
2021-12-26 20:28:54 +0200

    Update HashBrute.py

--- HashBrute.py
+++ HashBrute.py
@@ -1,3 +1,4 @@
+import binascii
 from termcolor import colored as c
 import hashlib as h
 import os.path
@@ -100,33 +101,82 @@ def md5(file, hsh):
         print(f"Error while decrypting !\nError code : {e}")
 
 
+def ntlm(file, hsh):
+    try:
+        i = 0
+        tim = time.time()
+        print(c("STARTED DECRYPTING ", "red") + c("NTLM", 'blue') + c(" - ", "red") + c(hsh, "green") + c(" FROM ", "red") + c(file, 'green'))
+        time.sleep(3)
+        with open(file, 'r') as filename:
+            for line in filename.readlines():
+                for passwd in line.split():
+                    i += 1
+                    pre = (i * 100) / counts
+                    dur = time.time() - tim
+                    print(c(f"\rTRIES : {i}/{counts}\t\t", "yellow") + c(f"PROGRESS : {round(pre, 2)}%\t\t", "blue") + c(f"DURATION : {round(dur, 2)}s", "green"), end=" ")
+                    y = h.new('md4', passwd.encode('utf-16le')).digest()
+                    x = binascii.hexlify(y).decode()
+                    if x == hsh:
+                        print(c("\nDecryption is over !\n", 'red') + c("Decrypted string : ", 'yellow') + c(passwd, 'green'))
+                        return True
+                    else:
+                        continue
+        return False
+    except Exception as e:
+        print(f"Error while decrypting !\nError code : {e}")
+
+
 def detect(hsh):
+    # 64 bits hash
     if len(hsh) == 64:
         print(c("SHA256 DETECTED !!!\n", 'blue'))
         time.sleep(1)
         if not sha256(wordlist, hsh):
-            print(c("NO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+            print(c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
             quit()
+    # 40 bits hash
     elif len(hsh) == 40:
         print(c("SHA1 DETECTED !!!\n", 'blue'))
         time.sleep(1)
         if not sha1(wordlist, hsh):
-            print(c("NO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+            print(c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
             quit()
+    # 32 bits hash
     elif len(hsh) == 32:
-        print(c("MD5 DETECTED !!!\n", 'blue'))
-        time.sleep(1)
-        if not md5(wordlist, hsh):
-            print(c("NO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
-            quit()
+        choice = input(c("32 bits hash was found, please choose hash algorithm :\n", 'red') + c("[NTLM/MD5 or * for both] : ", 'yellow'))
+        choices = ['ntlm', 'md5', '*']
+        while choice.lower() not in choices:
+            choice = input(c("WRONG INPUT !!!\n", 'red') + c("[NTLM/MD5/*] : ", 'yellow'))
+        if choice.lower() == choices[0]:
+            print(c("\nNTLM DETECTED !!!\n", 'blue'))
+            time.sleep(1)
+            if not ntlm(wordlist, hsh):
+                print(c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+                quit()
+        elif choice.lower() == choices[1]:
+            print(c("\nMD5 DETECTED !!!\n", 'blue'))
+            time.sleep(1)
+            if not md5(wordlist, hsh):
+                print(c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+                quit()
+        else:
+            print(c("\nTRYING BOTH ", 'red') + c("NTLM ", 'blue') + c("& ", "red") + c("MD5", 'blue'))
+            time.sleep(1)
+            if not ntlm(wordlist, hsh):
+                print(c("\n\nNOT NTLM !!!\n", "red") + c("TRYING MD5 !\n", 'yellow'))
+                time.sleep(2)
+                if not md5(wordlist, hsh):
+                    print(c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+                    quit()
+    # 128 bits hash
     elif len(hsh) == 128:
         print(c("SHA512 DETECTED !!!\n", 'blue'))
         time.sleep(1)
         if not sha512(wordlist, hsh):
-            print(c("NO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+            print(c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
             quit()
     else:
-        print(c("UNRECOGNIZED HASH FORMAT !\n", 'red') + c("Maybe hash has salt ?", 'yellow'))
+        print(c("\nUNRECOGNIZED HASH FORMAT !\n", 'red') + c("Maybe hash has salt ?", 'yellow'))
         quit()
 
 
