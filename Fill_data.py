8aac196fae40ec84b5ab035277df5ee8fa03f791
Akh4th <68875577+Akh4th@users.noreply.github.com>
2021-12-27 14:29:08 +0200

    Update Fill_data.py

--- Fill_data.py
+++ Fill_data.py
@@ -4,17 +4,8 @@ import hashlib as h
 import os.path
 
 choices = ['MD5', 'SHA1', 'SHA512', 'SHA256', 'NTLM']
-# Getting hash type
-kind = input(c("Hash Type : ", "blue"))
-while kind.upper() not in choices:
-    kind = input(c("Wrong hash type, try again ", "red"))
-# Importing wordlist
-wordlist = input(c("Wordlist name : ", "blue"))
-while not os.path.isfile(wordlist):
-    wordlist = input(c("File doesn't exist, try again ", "red"))
 
 
-# Checking if hash is already on database
 def check(kind1, word1):
     with open(kind1.upper() + "_table.txt", 'r') as file2:
         for word2 in file2.readlines():
@@ -23,47 +14,62 @@ def check(kind1, word1):
     return False
 
 
-if kind.lower() == "md5":
-    with open(wordlist, "r") as words:
-        for line in words.readlines():
-            with open(kind.upper() + "_table.txt", "a") as file:
-                x = h.md5(line.encode()).hexdigest()
-                if not check(kind, line):
-                    file.write(x + " = " + line)
+def matching(z):
+    match z:
+        case "md5":
+            print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("MD5", 'blue'))
+            with open(wordlist, "r") as words:
+                for line in words.readlines():
+                    with open(kind.upper() + "_table.txt", "a") as file:
+                        x = h.md5(line.encode()).hexdigest()
+                        if not check(kind, line):
+                            file.write(x + " = " + line)
+
+        case "sha1":
+            print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("SHA1", 'blue'))
+            with open(wordlist, "r") as words:
+                for line in words.readlines():
+                    with open(kind.upper() + "_table.txt", "a") as file:
+                        x = h.sha1(line.encode()).hexdigest()
+                        if not check(kind, line):
+                            file.write(x + " = " + line)
+
+        case "sha256":
+            print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("SHA256", 'blue'))
+            with open(wordlist, "r") as words:
+                for line in words.readlines():
+                    with open(kind.upper() + "_table.txt", "a") as file:
+                        x = h.sha256(line.encode()).hexdigest()
+                        if not check(kind, line):
+                            file.write(x + " = " + line)
+
+        case "sha512":
+            print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("SHA512", 'blue'))
+            with open(wordlist, "r") as words:
+                for line in words.readlines():
+                    with open(kind.upper() + "_table.txt", "a") as file:
+                        x = h.sha512(line.encode()).hexdigest()
+                        if not check(kind, line):
+                            file.write(x + " = " + line)
+
+        case "ntlm":
+            print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("NTLM", 'blue'))
+            with open(wordlist, "r") as words:
+                for line in words.readlines():
+                    with open(kind.upper() + "_table.txt", "a") as file:
+                        y = h.new('md4', line.encode('utf-16le')).digest()
+                        x = binascii.hexlify(y).decode()
+                        if not check(kind, line):
+                            file.write(x + " = " + line)
+    print(c("\nDONE !\n", "green") + c(z.upper() + "_table.txt", "yellow") + c(" Has been updated.", "green"))
 
 
-elif kind.lower() == "sha1":
-    with open(wordlist, "r") as words:
-        for line in words.readlines():
-            with open(kind.upper() + "_table.txt", "a") as file:
-                x = h.sha1(line.encode()).hexdigest()
-                if not check(kind, line):
-                    file.write(x + " = " + line)
-
-
-elif kind.lower() == "sha256":
-    with open(wordlist, "r") as words:
-        for line in words.readlines():
-            with open(kind.upper() + "_table.txt", "a") as file:
-                x = h.sha256(line.encode()).hexdigest()
-                if not check(kind, line):
-                    file.write(x + " = " + line)
-
-
-elif kind.lower() == "sha512":
-    with open(wordlist, "r") as words:
-        for line in words.readlines():
-            with open(kind.upper() + "_table.txt", "a") as file:
-                x = h.sha512(line.encode()).hexdigest()
-                if not check(kind, line):
-                    file.write(x + " = " + line)
-
-
-else:
-    with open(wordlist, "r") as words:
-        for line in words.readlines():
-            with open(kind.upper() + "_table.txt", "a") as file:
-                y = h.new('md4', line.encode('utf-16le')).digest()
-                x = binascii.hexlify(y).decode()
-                if not check(kind, line):
-                    file.write(x + " = " + line)
+# Getting hash type
+kind = input(c("Hash Type : ", "blue"))
+while kind.upper() not in choices:
+    kind = input(c("Wrong hash type, try again ", "red"))
+# Importing wordlist
+wordlist = input(c("Wordlist name : ", "blue"))
+while not os.path.isfile(wordlist):
+    wordlist = input(c("File doesn't exist, try again ", "red"))
+matching(kind.lower())
