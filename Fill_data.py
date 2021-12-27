48531bac81e540be7519bb056f8425a4f1a92460
Akh4th <68875577+Akh4th@users.noreply.github.com>
2021-12-27 14:45:42 +0200

    Update Fill_data.py

--- Fill_data.py
+++ Fill_data.py
@@ -6,11 +6,11 @@ import os.path
 choices = ['MD5', 'SHA1', 'SHA512', 'SHA256', 'NTLM']
 
 
-def check(kind1, word1):
+def check(kind1, hsh):
     try:
         with open(kind1.upper() + "_table.txt", 'r') as file2:
             for word2 in file2.readlines():
-                if word2.split(" ")[2] == word1:
+                if word2.split(" ")[0] == hsh:
                     return True
         return False
     except:
@@ -25,8 +25,8 @@ def matching(z):
                 for line in words.readlines():
                     with open(kind.upper() + "_table.txt", "a") as file:
                         x = h.md5(line.encode()).hexdigest()
-                        if not check(kind, line):
-                            file.write(x + " = " + line)
+                        if not check(kind, x):
+                            file.write(x + " = " + line + "\n")
 
         case "sha1":
             print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("SHA1", 'blue'))
@@ -34,8 +34,8 @@ def matching(z):
                 for line in words.readlines():
                     with open(kind.upper() + "_table.txt", "a") as file:
                         x = h.sha1(line.encode()).hexdigest()
-                        if not check(kind, line):
-                            file.write(x + " = " + line)
+                        if not check(kind, x):
+                            file.write(x + " = " + line + "\n")
 
         case "sha256":
             print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("SHA256", 'blue'))
@@ -43,8 +43,8 @@ def matching(z):
                 for line in words.readlines():
                     with open(kind.upper() + "_table.txt", "a") as file:
                         x = h.sha256(line.encode()).hexdigest()
-                        if not check(kind, line):
-                            file.write(x + " = " + line)
+                        if not check(kind, x):
+                            file.write(x + " = " + line + "\n")
 
         case "sha512":
             print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("SHA512", 'blue'))
@@ -52,8 +52,8 @@ def matching(z):
                 for line in words.readlines():
                     with open(kind.upper() + "_table.txt", "a") as file:
                         x = h.sha512(line.encode()).hexdigest()
-                        if not check(kind, line):
-                            file.write(x + " = " + line)
+                        if not check(kind, x):
+                            file.write(x + " = " + line + "\n")
 
         case "ntlm":
             print(c("\nConverting from ", "red") + c(wordlist, "blue"), c("to ", "red") + c("NTLM", 'blue'))
@@ -62,8 +62,8 @@ def matching(z):
                     with open(kind.upper() + "_table.txt", "a") as file:
                         y = h.new('md4', line.encode('utf-16le')).digest()
                         x = binascii.hexlify(y).decode()
-                        if not check(kind, line):
-                            file.write(x + " = " + line)
+                        if not check(kind, x):
+                            file.write(x + " = " + line + "\n")
     print(c("\nDONE !\n", "green") + c(z.upper() + "_table.txt", "yellow") + c(" Has been updated.", "green"))
 
 
