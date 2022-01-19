b4f9408d135f496eb729eefe58f90486d2f7496f
Akh4th <68875577+Akh4th@users.noreply.github.com>
2022-01-19 12:47:53 +0200

    Update HashBrute.py

--- HashBrute.py
+++ HashBrute.py
@@ -3,6 +3,15 @@ from termcolor import colored as c
 import hashlib as h
 import os.path
 import time
+import argparse
+
+
+p = argparse.ArgumentParser(description="Brute force hashes using wordlists and rainbow tables !")
+p.add_argument("--hash", help="Hash to brute force", nargs=1, type=str, metavar="")
+p.add_argument("--wordlist", help="Wordlist to brute force with", nargs=1, metavar="")
+p.add_argument("--check_db", help="Check if hash is on databases", action="store_true")
+p.add_argument("--type", help="Hash type if known", nargs=1, type=str, metavar="")
+args = p.parse_args()
 
 
 def check(kind, hsh):
@@ -23,7 +32,7 @@ def write(kind, hsh, word):
     with open(kind + "_table.txt", "r") as file:
         if hsh not in file.read():
             with open(kind + "_table.txt", "a") as file1:
-                file1.write(hsh + " = " + word)
+                file1.write(hsh + " = " + word + "\n")
                 file1.close()
             file.close()
 
@@ -219,26 +228,92 @@ def detect(hsh):
 
 
 if __name__ == "__main__":
-    print(c("Welcome to python ", "red") + c("HASH DECRYPTER ", "green") + c("by", "red") + c(" Akh4th", 'blue') + c(" !", 'red'))
-    time.sleep(3)
-    # Getting and validating hash file
-    hashed = input(c("Hash file name : ", 'yellow'))
-    while not os.path.isfile(hashed):
-        hashed = input(c("File doesn't exist, make sure you use full path !\n", 'red') + c("Hash file name : ", 'yellow'))
-    time.sleep(1)
-    # Getting and validating wordlist file
-    wordlist = input(c("Wordlist file name : ", 'yellow'))
-    while not os.path.isfile(wordlist):
-        wordlist = input(c("File doesn't exist, make sure you use full path !\n", 'red') + c("Hash file name : ", 'yellow'))
-    time.sleep(1)
-    # Storing the hash on a variable
-    with open(hashed, 'r') as hush:
-        hashed = hush.read()
-        print(c("\nHASH SUCCESSFULLY LOADED !", "green"))
-    time.sleep(1)
-    # counting tries on wordlist
-    counts = len(open(wordlist, "r").readlines())
-    print(c("WORDLIST SUCCESSFULLY LOADED !\n", "green") + c("TOTAL OF ", "red") + c(counts, "yellow") + c(" tries ahead !\n", "red"))
-    time.sleep(1)
-    # Starting brute force according to hash's length
-    detect(hashed)
+    if not args.hash and not args.wordlist:
+        print(
+            c("Welcome to python ", "red") + c("HASH DECRYPTER ", "green") + c("by", "red") + c(" Akh4th", 'blue') + c(
+                " !", 'red'))
+        time.sleep(3)
+        # Getting and validating hash file
+        hashed = input(c("Hash file name : ", 'yellow'))
+        while not os.path.isfile(hashed):
+            hashed = input(
+                c("File doesn't exist, make sure you use full path !\n", 'red') + c("Hash file name : ", 'yellow'))
+        time.sleep(1)
+        # Getting and validating wordlist file
+        wordlist = input(c("Wordlist file name : ", 'yellow'))
+        while not os.path.isfile(wordlist):
+            wordlist = input(
+                c("File doesn't exist, make sure you use full path !\n", 'red') + c("Hash file name : ", 'yellow'))
+        time.sleep(1)
+        # Storing the hash on a variable
+        with open(hashed, 'r') as hush:
+            hashed = hush.read()
+            print(c("\nHASH SUCCESSFULLY LOADED !", "green"))
+        time.sleep(1)
+        # counting tries on wordlist
+        counts = len(open(wordlist, "r").readlines())
+        print(c("WORDLIST SUCCESSFULLY LOADED !\n", "green") + c("TOTAL OF ", "red") + c(counts, "yellow") + c(
+            " tries ahead !\n", "red"))
+        time.sleep(1)
+        # Starting brute force according to hash's length
+        detect(hashed)
+    else:
+        try:
+            wordlist = args.wordlist[0]
+            hashed = args.hash[0]
+            if not args.type:
+                detect(hsh=hashed)
+            else:
+                hsh = args.type[0]
+                hash_types = ["MD5", "SHA512", "SHA256", "SHA1", "NTLM"]
+                if hsh.upper() not in hash_types:
+                    print("Wrong hash type given, please try again.")
+                    quit()
+                else:
+                    if hsh == hash_types[0]:
+                        if args.check_db:
+                            if check(hsh, hashed):
+                                quit()
+                            else:
+                                if not md5(wordlist, hsh):
+                                    print(c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+                                    quit()
+                    elif hsh == hash_types[1]:
+                        if args.check_db:
+                            if check(hsh, hashed):
+                                quit()
+                            else:
+                                if not sha512(wordlist, hsh):
+                                    print(
+                                        c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+                                    quit()
+                    elif hsh == hash_types[2]:
+                        if args.check_db:
+                            if check(hsh, hashed):
+                                quit()
+                            else:
+                                if not sha256(wordlist, hsh):
+                                    print(
+                                        c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+                                    quit()
+                    elif hsh == hash_types[3]:
+                        if args.check_db:
+                            if check(hsh, hashed):
+                                quit()
+                            else:
+                                if not sha1(wordlist, hsh):
+                                    print(
+                                        c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+                                    quit()
+                    else:
+                        if args.check_db:
+                            if check(hsh, hashed):
+                                quit()
+                            else:
+                                if not ntlm(wordlist, hsh):
+                                    print(
+                                        c("\nNO MATCH FOUND !\n", "red") + c("PLEASE USE ANOTHER WORDLIST !", 'yellow'))
+                                    quit()
+        except Exception as e:
+            print("There was an error while running, please try again.\nError code : " + str(e))
+            quit()
